4e61c71e0dd4adf5883c6e55af36ba3200c845ce
Frank Bennett <biercenator@gmail.com>
2011-08-10 20:46:03 +0000

    By golly, it seems to work.

--- python/zotero.py
+++ python/zotero.py
@@ -14,94 +14,112 @@ from docutils.transforms import TransformError, Transform
 from urllib import unquote
 
 citation_format = "http://www.zotero.org/styles/chicago-author-date"
+
+# unique items
 item_list = []
 item_array = {}
-note_number = 1
+
+# everything, in sequence
+cite_list = []
+cite_pos = 0
+
+# variables for use in final assignment of note numbers
+# (see CitationVisitor)
+in_note = False
+note_count = 0
+
+# placeholder for global bridge to Zotero
 zotero_thing = None;
+
+# verbose flag
+verbose_flag = False
+
+class CitationVisitor(nodes.SparseNodeVisitor):
+
+    def visit_pending(self, node):
+        global cite_list, cite_pos, in_note, note_count
+        if node.details.has_key('zoteroCitation'):
+            # Do something about the number
+            if in_note:
+                cite_list[cite_pos]['noteIndex'] = note_count
+            cite_pos += 1
+    def depart_pending(self, node):
+        pass
+
+    def visit_footnote(self, node):
+        global in_note, note_count
+        in_note = True
+        note_count += 1
+        
+    def depart_footnote(self, node):
+        global in_note
+        in_note = False
+
+    def visit_paragraph(self, node):
+        # Should be able to insert magic here that
+        # converts in-footnote paras between
+        # citations to inline. Could also look at
+        # merging adjacent cites into proper multiple
+        # citations -- tricky, but we have full control
+        # in this phase of processing.
+        pass
+    def depart_paragraph(self, node):
+        pass
 
 class ZoteroConnection(object):
     def __init__(self, **kwargs):
-        self.bibType = kwargs.get('bibType',
-                                  citation_format)
+        self.bibType = kwargs.get('bibType', citation_format)
         self.firefox_connect()
         self.zotero_resource()
 
     def firefox_connect(self):
-        # get the bridge and the back-channel
-        self.back_channel, self.bridge = wait_and_create_network("127.0.0.1",
-                                                                          24247)
-
+        self.back_channel, self.bridge = wait_and_create_network("127.0.0.1", 24247)
         self.back_channel.timeout = self.bridge.timeout = 60
 
     def zotero_resource(self):
         self.methods = JSObject(self.bridge, "Components.utils.import('resource://zotero-for-restructured-text/modules/export.js')")
 
-class ZoteroSetupTransformDirective(Transform):
-    default_priority = 500
-    def apply(self):
-        global zotero_thing
-        # Do stuff before trashing the node
-        print "================ Setup run #2 (load IDs to processor) =============="
-        ##self.state_machine.document['zotero_connection'].reStructuredCSL.updateItems(item_list)
-        zotero_thing.registerItemIds(item_list)
-        self.startnode.parent.remove(self.startnode)
-        # Do stuff after trashing the node
-    
 
 class ZoteroSetupDirective(Directive, ZoteroConnection):
-
     from docutils.parsers.rst.directives import unchanged
-
-    def __init__(self, one, two, three, four, five, six, seven, eight, nine):
-        global zotero_thing
-        # There MUST be a better way to do this.
-        Directive.__init__(self, one, two, three, four, five, six, seven, eight, nine)
+    def __init__(self, *args):
+        global zotero_thing, verbose_flag
+        Directive.__init__(self, *args)
         # This is necessary: connection hangs if created outside of an instantiated
         # directive class.
         ZoteroConnection.__init__(self)
-
-        ##self.state_machine.document['zotero_connection'] = self.methods
         zotero_thing = self.methods
-        #print 'Register on state machine: %s' % self.state_machine.document['zotero_connection']
+        verbose_flag = self.state_machine.reporter.report_level
 
     required_arguments = 0
     optional_arguments = 0
     has_content = False
     option_spec = {'format': unchanged}
     def run(self):
-        global citation_format, zotero_thing
-        print "================ Setup run #1 (establish connection, spin up processor) =============="
+        global citation_format, zotero_thing, verbose_flag
+        if verbose_flag:
+            print "=== Zotero4reST: Setup run #1 (establish connection, spin up processor) ==="
         if self.options.has_key('format'):
             citation_format = self.options['format']
-        ##self.state_machine.document['zotero_connection'].instantiateCiteProc(citation_format);
         zotero_thing.instantiateCiteProc(citation_format);
         pending = nodes.pending(ZoteroSetupTransformDirective)
         pending.details.update(self.options)
         self.state_machine.document.note_pending(pending)
         return [pending]
 
-class ZoteroTransformDirective(Transform):
-    default_priority = 510
-
-    def unquote_u(self,source):
-        res = unquote(source)
-        if '%u' in res:
-            res = res.replace('%u','\\u').decode('unicode_escape')
-        return res
-
+class ZoteroSetupTransformDirective(Transform):
+    default_priority = 500
     def apply(self):
-        global note_number
-        print "================ Citation run #2 (render cite and insert) ================"
-        ##
-        ## Need to figure out whether we're in a note and what the note number is somehow.
-        ## When that's been sorted ... we're done!
-        ##
-        res = zotero_thing.getCitationBlock({'citationItems':[{'id':66}],'properties':{'noteIndex':note_number}})
-        note_number += 1
-        mystr = self.unquote_u(res)
-        # Don't know what the empty string as first argument is for.
-        newnode = nodes.generated('', mystr)
-        self.startnode.replace_self(newnode)
+        global zotero_thing, cite_pos, verbose_flag
+        if verbose_flag:
+            print "=== Zotero4reST: Setup run #2 (load IDs to processor) ==="
+        zotero_thing.registerItemIds(item_list)
+        self.startnode.parent.remove(self.startnode)
+        ## Here we walk the document, checking note state and
+        ## setting noteIndex value as we go along.
+        visitor = CitationVisitor(self.document)
+        self.document.walkabout(visitor)
+        cite_pos = 0
 
 class ZoteroDirective(Directive):
     """
@@ -125,19 +143,73 @@ class ZoteroDirective(Directive):
                    'suppress-author': directives.flag}
 
     def run(self):
-        global citation_format, item_list, item_array, zotero_thing
-        #print '  Found on state machine: %s' % self.state_machine.document['zotero_connection']
-        #print 'Argh! %s' % self.arguments[0]
-        # Arrange to complain if zotero_setup:: declaration was missing.
-        print "================ Citation run #1 (record ID) ================"
-        ##itemID = int(self.state_machine.document['zotero_connection'].getItemId(self.arguments[0]))
+        global citation_format, item_list, item_array, zotero_thing, cite_list, verbose_flag
+        # XXX Should complain if zotero_setup:: declaration was missing.
+        if verbose_flag:
+            print "--- Zotero4reST: Citation run #1 (record ID) ---"
         itemID = int(zotero_thing.getItemId(self.arguments[0]))
+        for key in ['locator', 'label', 'prefix', 'suffix']:
+            if not self.options.has_key(key):
+                self.options[key] = ''
+        details = {
+            'itemID':itemID,
+            'noteIndex':0,
+            'indexNumber': len(cite_list),
+            'locator': self.options['locator'],
+            'label': self.options['label'],
+            'prefix': self.options['prefix'],
+            'suffix': self.options['suffix']
+        }
         if not item_array.has_key(itemID):
             item_array[itemID] = True
             item_list.append(itemID)
-        # Probably no need to stash anything in the options, but there they are.
-        #print self.options
-        pending = nodes.pending(ZoteroTransformDirective, {'notenum': 1}, rawsource="big *bogus* thing")
+        cite_list.append(details)
+        pending = nodes.pending(ZoteroTransformDirective)
         pending.details.update(self.options)
+        pending.details['zoteroCitation'] = True
         self.state_machine.document.note_pending(pending)
         return [pending]
+
+class ZoteroTransformDirective(Transform):
+    default_priority = 510
+    # Bridge hangs if output contains above-ASCII chars (I guess Telnet kicks into
+    # binary mode in that case, leaving us to wait for a null string terminator)
+    # JS strings are in Unicode, and the JS escaping mechanism for Unicode with
+    # escape() is, apparently, non-standard. I played around with various
+    # combinations of decodeURLComponent() / encodeURIComponent() and escape() /
+    # unescape() ... applying escape() on the JS side of the bridge, and
+    # using the following suggestion for a Python unquote function worked,
+    # so I stuck with it:
+    #   http://stackoverflow.com/questions/300445/how-to-unquote-a-urlencoded-unicode-string-in-python
+    def unquote_u(self,source):
+        res = unquote(source)
+        if '%u' in res:
+            res = res.replace('%u','\\u').decode('unicode_escape')
+        return res
+
+    def apply(self):
+        global note_number, cite_pos, cite_list, verbose_flag
+        if verbose_flag:
+            print "--- Zotero4reST: Citation run #2 (render cite and insert) ---"
+        details = cite_list[cite_pos]
+        citation = {
+            'citationItems':[
+                {
+                    'id': details['itemID'],
+                    'prefix': details['prefix'],
+                    'suffix': details['suffix'],
+                    'locator': details['locator'],
+                    'label': details['label']
+                }
+            ],
+            'properties': {
+                'index': cite_pos,
+                'noteIndex': details['noteIndex']
+            }
+        }
+        res = zotero_thing.getCitationBlock(citation)
+        cite_pos += 1
+        mystr = self.unquote_u(res)
+        # Don't know what the empty string as first argument is for.
+        newnode = nodes.generated('', mystr)
+        self.startnode.replace_self(newnode)
--- sample.rst
+++ sample.rst
@@ -5,12 +5,19 @@
 .. zotero_setup::
    :format: chicago-note
 
-This is an example document which cites one source. [#]_ And another source. [#]_
+This is an example document which cites one source. [#]_ And another source. [#]_ And another. [#]_
 
 .. [#]
   .. zotero:: DDW6JB2S   Brooker, Charlie. “The news coverage of the Norway mass-killings was fact-free conjecture.” The Guardian. London, July 24, 2011. http://www.guardian.co.uk/commentisfree/2011/jul/24/charlie-brooker-norway-mass-killings.
+     :prefix: Try that! See, e.g. 
 
 
 .. [#]
-  .. zotero:: DDW6JB2S   Brooker, Charlie. “The news coverage of the Norway mass-killings was fact-free conjecture.” The Guardian. London, July 24, 2011. http://www.guardian.co.uk/commentisfree/2011/jul/24/charlie-brooker-norway-mass-killings.
+   .. zotero:: DDW6JB2S   Brooker, Charlie. “The news coverage of the Norway mass-killings was fact-free conjecture.” The Guardian. London, July 24, 2011. http://www.guardian.co.uk/commentisfree/2011/jul/24/charlie-brooker-norway-mass-killings.
 
+   my aunt sally
+
+   .. zotero:: DDW6JB2S   Brooker, Charlie. “The news coverage of the Norway mass-killings was fact-free conjecture.” The Guardian. London, July 24, 2011. http://www.guardian.co.uk/commentisfree/2011/jul/24/charlie-brooker-norway-mass-killings.
+
+.. [#]
+   .. zotero:: MAJBZZZ4   Swan, Michael. Practical English Usage. 3rd ed. Oxford University Press, USA, 2005.  
