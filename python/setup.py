c51ea9f4c69fcb860385b2c5e9f8b91fa6fd806d
Erik Hetzner <egh@e6h.org>
2011-09-01 17:39:02 -0700

    add requires for setup.py

--- python/setup.py
+++ python/setup.py
@@ -1,19 +1,19 @@
 #!/usr/bin/env python
 
-try:
-    from distutils.core import setup
-except ImportError:
-    from setuptools import setup
+from setuptools import setup
 
-setup(name         = 'zotero4rst',
-      version      = '0.1',
-      description  = 'Zotero for reStructuredText',
-      author       = 'Erik Hetzner',
-      author_email = 'egh@e6h.org',
-      url          = 'http://bitbucket.org/egh/zotero-plain/',
-      packages     = ['zotero4rst'],
-      scripts      = ['bin/zrst2html',
-                      'bin/zrst2odt', 
-                      'bin/zrst2pdf',
-                      'bin/zrst2pseudoxml'],
+setup(name             = 'zotero4rst',
+      version          = '0.1',
+      description      = 'Zotero for reStructuredText',
+      author           = 'Erik Hetzner',
+      author_email     = 'egh@e6h.org',
+      url              = 'http://bitbucket.org/egh/zotero-plain/',
+      packages         = ['zotero4rst'],
+      install_requires = ["BeautifulSoup>=3.2.0",
+                          "docutils==0.8.1",
+                          "jsbridge>=2.4.4"],
+      scripts          = ['bin/zrst2html',
+                          'bin/zrst2odt', 
+                          'bin/zrst2pdf',
+                          'bin/zrst2pseudoxml']
       )
