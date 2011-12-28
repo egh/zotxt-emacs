c8dc5700f5fbb4271d8321ca7f561c3ade54c375
Erik Hetzner <egh@e6h.org>
2011-12-27 16:51:39 -0800

    add CitationCluster class

--- python/xciterst/__init__.py
+++ python/xciterst/__init__.py
@@ -1,5 +1,7 @@
 class CitationInfo(object):
-    """Class to hold information about a citation for passing to citeproc."""
+    """Class to hold information about a citation for passing to
+    citeproc."""
+
     def __init__(self, **kwargs):
         self.key = kwargs.get('key')
         self.label = kwargs.get('label', None)
@@ -9,3 +11,12 @@ class CitationInfo(object):
         self.suffix = kwargs.get('suffix', None)
         self.author_only = kwargs.get('author_only', False)
         self.id = None
+
+class CitationCluster(object):
+    """Class to hold a cluster of citations, with information about
+    them suitable for submission to citeproc."""
+
+    def __init__(self, citations):
+        self.citations = citations
+        self.note_index = 0
+        self.index = 0
