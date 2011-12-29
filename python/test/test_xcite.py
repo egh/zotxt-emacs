820e251c42b76e90725747d21c1789968d3bbe39
Erik Hetzner <egh@e6h.org>
2011-12-28 22:37:47 -0800

    make parse simpler in test

--- python/test/test_xcite.py
+++ python/test/test_xcite.py
@@ -5,11 +5,14 @@ import unittest2
 
 class TestXciteParserp(unittest2.TestCase):
     def setUp(self):
-        pass
-    
+        self.parser = CiteParser()
+
+    def parse(self, parse_string):
+        return self.parser.parse(parse_string)
+        
     # @item1
     def test_parse_1(self):
-        [first_cluster, second_cluster] = CiteParser().parse("@item1")
+        [first_cluster, second_cluster] = self.parse("@item1")
         self.assertEqual(first_cluster, 
                          [CitationInfo(key="item1", author_only=True)])
         self.assertEqual(second_cluster, 
@@ -17,7 +20,7 @@ class TestXciteParserp(unittest2.TestCase):
 
     # @item1, [p. 30]
     def test_parse_2(self):
-        [first_cluster, second_cluster] = CiteParser().parse("@item1 [p. 30]")
+        [first_cluster, second_cluster] = self.parse("@item1 [p. 30]")
         self.assertEqual(first_cluster,
                          [CitationInfo(key="item1", author_only=True)])
         self.assertEqual(second_cluster,
@@ -26,7 +29,7 @@ class TestXciteParserp(unittest2.TestCase):
     # @item1 [p. 30, with suffix]
     # XXX - is this parse right?
     def test_parse_3(self):
-        [first_cluster, second_cluster] = CiteParser().parse("@item1 [p. 30, with suffix]")
+        [first_cluster, second_cluster] = self.parse("@item1 [p. 30, with suffix]")
         self.assertEqual(first_cluster,
                          [CitationInfo(key="item1", author_only=True)])
         self.assertEqual(second_cluster,
@@ -37,7 +40,7 @@ class TestXciteParserp(unittest2.TestCase):
                          
     # @item1 [-@item2 p. 30; see also @item3]
     def test_parse_4(self):
-        [first_cluster, second_cluster] = CiteParser().parse("@item1 [-@item2 p. 30; see also @item3]")
+        [first_cluster, second_cluster] = self.parse("@item1 [-@item2 p. 30; see also @item3]")
         self.assertEqual(first_cluster,
                          [CitationInfo(key="item1", author_only=True)])
         self.assertEqual(second_cluster,
@@ -51,7 +54,7 @@ class TestXciteParserp(unittest2.TestCase):
 
     # [see @item1 p. 34-35; also @item3 chap. 3]
     def test_parse_5(self):
-        [first_cluster, second_cluster] = CiteParser().parse("[see @item1 p. 34-35; also @item3 chap. 3]")
+        [first_cluster, second_cluster] = self.parse("[see @item1 p. 34-35; also @item3 chap. 3]")
         self.assertEqual(first_cluster, None)
         self.assertEqual(second_cluster,
                          [CitationInfo(key="item1",
@@ -63,7 +66,7 @@ class TestXciteParserp(unittest2.TestCase):
 
     # [see @item1 p. 34-35]
     def test_parse_6(self):
-        [first_cluster, second_cluster] = CiteParser().parse("[see @item1 p. 34-35]")
+        [first_cluster, second_cluster] = self.parse("[see @item1 p. 34-35]")
         self.assertEqual(first_cluster, None)
         self.assertEqual(second_cluster,
                          [CitationInfo(key="item1",
@@ -72,7 +75,7 @@ class TestXciteParserp(unittest2.TestCase):
 
     # [@item1 pp. 33, 35-37 and nowhere else]
     def test_parse_7(self):
-        [first_cluster, second_cluster] = CiteParser().parse("[@item1 pp. 33, 35-37 and nowhere else]")
+        [first_cluster, second_cluster] = self.parse("[@item1 pp. 33, 35-37 and nowhere else]")
         self.assertEqual(first_cluster, None)
         self.assertEqual(second_cluster,
                          [CitationInfo(key="item1",
@@ -81,7 +84,7 @@ class TestXciteParserp(unittest2.TestCase):
         
     # [@item1 and nowhere else]
     def test_parse_8(self):
-        [first_cluster, second_cluster] = CiteParser().parse("[@item1 and nowhere else]")
+        [first_cluster, second_cluster] = self.parse("[@item1 and nowhere else]")
         self.assertEqual(first_cluster, None)
         self.assertEqual(second_cluster,
                          [CitationInfo(key="item1",
@@ -90,7 +93,7 @@ class TestXciteParserp(unittest2.TestCase):
     # XXX BROKEN
     # [*see* @item1 p. **32**]
     # def test_parse_9(self):
-    #    [first_cluster, second_cluster] = CiteParser().parse("[*see* @item1 p. **32**]")
+    #    [first_cluster, second_cluster] = self.parse("[*see* @item1 p. **32**]")
     #    self.assertEqual(first_cluster, None)
     #    self.assertEqual(second_cluster,
     #                     [CitationInfo(key="item1",
@@ -99,14 +102,14 @@ class TestXciteParserp(unittest2.TestCase):
         
     # [@item3]
     def test_parse_10(self):
-        [first_cluster, second_cluster] = CiteParser().parse("[@item3]")
+        [first_cluster, second_cluster] = self.parse("[@item3]")
         self.assertEqual(first_cluster, None)
         self.assertEqual(second_cluster,
                          [CitationInfo(key="item3")])
 
     # [see @item2 chap. 3; @item3; @item1]
     def test_parse_11(self):
-        [first_cluster, second_cluster] = CiteParser().parse("[see @item2 chap. 3; @item3; @item1]")
+        [first_cluster, second_cluster] = self.parse("[see @item2 chap. 3; @item3; @item1]")
         self.assertEqual(first_cluster, None)
         self.assertEqual(second_cluster,
                          [CitationInfo(key="item2",
@@ -117,7 +120,7 @@ class TestXciteParserp(unittest2.TestCase):
 
     # [-@item1]
     def test_parse_12(self):
-        [first_cluster, second_cluster] = CiteParser().parse("[-@item1]")
+        [first_cluster, second_cluster] = self.parse("[-@item1]")
         self.assertEqual(first_cluster, None)
         self.assertEqual(second_cluster,
                          [CitationInfo(key="item1",
@@ -125,7 +128,7 @@ class TestXciteParserp(unittest2.TestCase):
 
     # [-@item2 p. 44]
     def test_parse_13(self):
-        [first_cluster, second_cluster] = CiteParser().parse("[-@item2 p. 44]")
+        [first_cluster, second_cluster] = self.parse("[-@item2 p. 44]")
         self.assertEqual(first_cluster, None)
         self.assertEqual(second_cluster,
                          [CitationInfo(key="item2",
@@ -134,7 +137,7 @@ class TestXciteParserp(unittest2.TestCase):
 
     # [@item1, p. 30]
     def test_parse_14(self):
-        [first_cluster, second_cluster] = CiteParser().parse("[@item1, p. 30]")
+        [first_cluster, second_cluster] = self.parse("[@item1, p. 30]")
         self.assertEqual(first_cluster, None)
         self.assertEqual(second_cluster,
                          [CitationInfo(key="item1",
