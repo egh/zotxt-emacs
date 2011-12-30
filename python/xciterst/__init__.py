0f6622b0e8185868a6ea38317f8ddfc3e5df1117
Frank Bennett <biercenator@gmail.com>
2011-12-30 22:43:12 +0000

    Punctuation following italics or bold attract an intervening space in the parser. This is a small hack to tidy up a comma, which is the most common case.

--- python/xciterst/__init__.py
+++ python/xciterst/__init__.py
@@ -1,3 +1,5 @@
+import re
+
 class CitationInfo(object):
     """Class to hold information about a citation for passing to citeproc."""
     def __init__(self, **kwargs):
@@ -6,6 +8,10 @@ class CitationInfo(object):
         self.locator = kwargs.get('locator', None)
         self.suppress_author = kwargs.get('suppress_author', False)
         self.prefix = kwargs.get('prefix', None)
+        if self.prefix:
+            self.prefix = re.sub(r'\s+,', ',', self.prefix)
         self.suffix = kwargs.get('suffix', None)
+        if self.suffix:
+            self.suffix = re.sub(r'\s+,', ',', self.suffix)
         self.author_only = kwargs.get('author_only', False)
         self.id = None
