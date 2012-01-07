b3611be474f6879fc506c92aaddd8ed2b49bf0d1
Erik Hetzner <egh@e6h.org>
2012-01-07 10:25:22 -0800

    add test for CitationCluster encoding

--- python/test/test_jsonencoder.py
+++ python/test/test_jsonencoder.py
@@ -1,5 +1,5 @@
 from zot4rst.jsonencoder import ZoteroJSONEncoder
-from xciterst import CitationInfo
+from xciterst import CitationCluster, CitationInfo
 import unittest2
 
 class TestJsonencoder(unittest2.TestCase):
@@ -7,7 +7,10 @@ class TestJsonencoder(unittest2.TestCase):
         self.encoder = ZoteroJSONEncoder()
         self.citation = CitationInfo(key="foo")
         self.citation.id = "foo"
-        
+        citation2 = CitationInfo(key="bar")
+        citation2.id = "bar"
+        self.citation_cluster = CitationCluster([self.citation, citation2])
+
     def test_basic(self):
         json = self.encoder.encode(self.citation)
         self.assertEqual(json, '{"id": "foo"}')
@@ -28,3 +31,9 @@ class TestJsonencoder(unittest2.TestCase):
         self.citation.locator = "p. 10"
         json = self.encoder.encode(self.citation)
         self.assertEqual(json, '{"locator": "p. 10", "prefix": "see ", "id": "foo", "suffix": " and nowhere else"}')
+
+    def test_cluster(self):
+        self.citation_cluster.index = 2
+        self.citation_cluster.note_index = 3
+        json = self.encoder.encode(self.citation_cluster)
+        self.assertEqual(json, '{"citationItems": [{"id": "foo"}, {"id": "bar"}], "properties": {"index": 2, "noteIndex": 3}}')
