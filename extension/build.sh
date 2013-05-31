4a5b714d5a2b6dae49c29014bfd3195d5f831272
Erik Hetzner <egh@e6h.org>
2013-05-30 20:53:53 -0700

    python bits moved to zot4rst

--- extension/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-find . -name '.hg' -prune -o \
-       -name '.hgignore' -prune -o \
-       -name 'build.sh' -prune -o \
-       -name '*~' -prune -o \
-       -print | xargs zip zotero-for-restructured-text.xpi
