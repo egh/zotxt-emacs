ff3f9c442ef6b3b847e76fe04a65aedc60176b10
Erik Hetzner <egh@e6h.org>
2013-06-01 10:28:34 -0700

    use zotxt, not mozrepl; rename (maybe?) to zotmacs

--- README.rst
+++ README.rst
@@ -1,23 +1,21 @@
-=========================================
- Zotero for plain text: ``zotero-plain``
-=========================================
+=========
+ zotmacs
+=========
 
-``zot4rst`` (zotero reStructuredText tools have been moved to
-`zot4rst`_. This repository now contains only some tools for emacs.
+Note: zotero-plain has been split into two parts: zotmacs (this
+repository) for working with org, and ``zot4rst`` (zotero
+reStructuredText tools), which been moved to `zot4rst`_.
 
 Emacs integration
 -----------------
 
 Emacs integration has 2 parts: a core library (``zotero.el``),
 `org-mode`_ integration (``org-zotero.el``). Emacs integration depends
-on the ``moz.el`` file from the mozrepl_ project, which can be
-retrieved at
-https://github.com/bard/mozrepl/blob/master/chrome/content/moz.el.
+on the zotxt_ Zotero extension.
 
 To install, add the following to your ``.emacs`` file::
 
-  (add-to-list 'load-path "/path/to/moz.el")
-  (add-to-list 'load-path "/path/to/zotero-plain/elisp/")
+  (add-to-list 'load-path "/path/to/zotmacs/elisp/")
   (autoload 'org-zotero-mode "org-zotero" "" t)
 
 zotero + org-mode
@@ -28,7 +26,7 @@ To insert a citation into a `org-mode`_ document, first enable the
 
   M-x org-zotero-mode
 
-Then select one or more items ina Zotero_ pane. Finally, in emacs_,
+Then select one or more items in Zotero_ pane. Finally, in emacs_,
 use: ``C-c z i`` to insert these items as citations. This inserts a
 zotero link with descriptive link text.
 
@@ -44,7 +42,7 @@ browser called by emacs, you may need to run the following command::
   xdg-mime default firefox.desktop x-scheme-handler/zotero
 
 .. _Zotero: http://www.zotero.org/
-.. _mozrepl: https://github.com/bard/mozrepl/wiki
 .. _emacs: http://www.gnu.org/software/emacs/
 .. _`org-mode`: http://orgmode.org/
 .. _`zot4rst`: http://bitbucket.org/egh/zot4rst
+.. _zotxt: http://bitbucket.org/egh/zotxt
--- elisp/zotero.el
+++ elisp/zotero.el
@@ -1,4 +1,4 @@
-(require 'moz)
+(require 'url-handlers)
 
 (defvar zotero-default-bibliography-style
   "http://www.zotero.org/styles/chicago-note-bibliography"
@@ -13,75 +13,24 @@
     (setq retval (replace-regexp-in-string "\^]" "”" retval))
     retval))
 
-(defun zotero-reset-mozrepl ()
-  (delete-process (inferior-moz-process)))
-
-(defun zotero-send-string (str)
-  (comint-send-string (inferior-moz-process) str))
-
-(defun zotero-send-strings (str-list)
-  (mapc 'zotero-send-string str-list))
-
-(defun zotero-js-write-to-file (filename expr)
-  (let ((js-func-def "
-function writeFile(filename, data) {
-  var file = Components.classes[\"@mozilla.org/file/local;1\"].createInstance(Components.interfaces.nsILocalFile);
-  var foStream = Components.classes[\"@mozilla.org/network/file-output-stream;1\"].createInstance(Components.interfaces.nsIFileOutputStream);
-  file.initWithPath(filename); 
-  foStream.init(file, 0x02 | 0x08, 00666, 0);
-  foStream.write(data, data.length);
-  foStream.close();
-};"))
-    (zotero-reset-mozrepl)
-    (zotero-send-strings 
-     (list js-func-def
-           (format "writeFile(\"%s\", %s);" filename expr)))))
-
-(defun zotero-js-get-string-value (expr)
-  (let ((tmp-file (make-temp-file "zotero")))
-    (delete-file tmp-file) ;; delete our version
-    (zotero-js-write-to-file tmp-file expr)
-    (while (not (file-exists-p tmp-file))
-      (sleep-for 0 10))
-    (prog1 (save-excursion
-             (with-temp-buffer
-               (insert-file-contents tmp-file)
-               (buffer-substring-no-properties (point-min) (point-max))))
-      (delete-file tmp-file))))
-  
 (defun zotero-generate-bib-entry-from-id (item-id &optional style bib-format)
-  (let* ((bib-type (format "bibliography=%s"
-                           (or style zotero-default-bibliography-style)))
-         (bib-format (or bib-format "text")))
-    (if (not (string-match "^[0-9]+_" item-id))
-        (setq item-id (format "0_%s" item-id)))
-    (zotero-reset-mozrepl)
-    (zotero-send-strings
-     `("var Zotero = Components.classes[\"@zotero.org/Zotero;1\"] .getService(Components.interfaces.nsISupports).wrappedJSObject;"
-       ,(format "var lkh = Zotero.Items.parseLibraryKeyHash(\"%s\");" item-id)
-       "var item = Zotero.Items.getByLibraryAndKey(lkh.libraryID, lkh.key);"
-       ,(format 
-          "var biblio = Zotero.QuickCopy.getContentFromItems(new Array(item), \"%s\");" 
-          bib-type)))
-    (zotero-clean-bib-entry
-     (zotero-js-get-string-value (format "biblio.%s" bib-format)))))
+  (let ((url (url-encode-url (format "http://localhost:23119/zotxt/items?key=%s&format=bibliography&style=%s"
+                                     item-id (or style zotero-default-bibliography-style)))))
+    (save-excursion
+      (with-temp-buffer
+        (url-insert (url-retrieve-synchronously url))
+        (beginning-of-buffer)
+        (let* ((data (json-read))
+               (first (elt data 0))
+               (text (cdr (assq 'text first))))
+          (zotero-clean-bib-entry text))))))
 
 (defun zotero-get-selected-item-ids ()
-  (zotero-reset-mozrepl)
-  (zotero-send-strings
-   `("var ZoteroPane = Components.classes[\"@mozilla.org/appshell/window-mediator;1\"] .getService(Components.interfaces.nsIWindowMediator).getMostRecentWindow(\"navigator:browser\").ZoteroPane;"
-     "var selected_items = ZoteroPane.getSelectedItems();"))
-  (let ((item-count (string-to-int
-                     (zotero-js-get-string-value "new String(selected_items.length)")))
-        (retval nil))
-    (dotimes (i item-count)
-      (let ((library-id (zotero-js-get-string-value 
-                         (format "selected_items[%s].libraryID" i)))
-            (item-key (zotero-js-get-string-value 
-                       (format "selected_items[%s].key" i))))
-        (if (string= library-id "")
-            (setq library-id "0"))
-      (setq retval (cons (format "%s_%s" library-id item-key) retval))))
-    retval))
+  (let ((url (url-encode-url "http://localhost:23119/zotxt/items?selected=selected&format=key")))
+    (save-excursion
+      (with-temp-buffer
+        (url-insert (url-retrieve-synchronously url))
+        (beginning-of-buffer)
+        (json-read)))))
 
 (provide 'zotero)
