6fad882ae46bb0101a9beb7c88a5f67cc8c9ba5c
Erik Hetzner <egh@e6h.org>
2014-08-16 09:57:18 -0700

    tests for deferred easykey functions

--- testing/zotxt-test.el
+++ testing/zotxt-test.el
@@ -1,5 +1,35 @@
-(ert-deftest zotxt-test-url-retrieve ()
-  (should (equal (zotxt-url-retrieve "http://127.0.0.1:23119/zotxt/items?key=0_ZBZQ4KMP&format=key") ["0_ZBZQ4KMP"])))
+(ert-deftest zotxt-test-get-item-easykey ()
+  (should (equal
+           (plist-get (zotxt-get-item-easykey '(:key "0_ZBZQ4KMP")) :easykey)
+           "doe:2005first")))
 
-(ert-deftest zotxt-test-easykey-get-item-easykey ()
-  (should (equal (zotxt-easykey-get-item-easykey "0_ZBZQ4KMP") "doe:2005first")))
+(ert-deftest zotxt-test-get-item-easykey-deferred ()
+  (should (equal
+           (let ((item
+                  (deferred:$
+                    (deferred:next
+                      (lambda ()
+                        '(:key "0_ZBZQ4KMP")))
+                    (deferred:nextc it
+                      (lambda (item)
+                        (zotxt-get-item-easykey-deferred item)))
+                    (deferred:sync! it))))
+             (plist-get item :easykey))
+           "doe:2005first")))
+
+(ert-deftest zotxt-test-get-item-easykey-list-chain ()
+  (should (equal
+           (let* ((items
+                  (deferred:$
+                    (deferred:next
+                      (lambda ()
+                        '((:key "0_ZBZQ4KMP")
+                          (:key "0_TWCW4IJ7"))))
+                    (deferred:nextc it
+                      (lambda (items)
+                        (zotxt-mapcar-deferred items #'zotxt-get-item-easykey-deferred)))
+                    (deferred:sync! it)))
+                  (easykeys (mapcar (lambda (item)
+                                      (plist-get item :easykey)) items)))
+             (sort easykeys #'string-lessp))
+           '("doe:2005first" "doe:2007why"))))
