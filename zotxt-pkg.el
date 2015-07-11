ffb7406413d6c8b0f90b899d1bd20680a7a21b7f
Erik Hetzner <egh@e6h.org>
2015-07-10 22:43:02 -0700

    Require dash

--- zotxt-pkg.el
+++ zotxt-pkg.el
@@ -1,3 +1,4 @@
 (define-package "zotxt" "0.1.16"
   "Tools to integrate emacs with Zotero via the zotxt plugin."
-  '((request-deferred "0.2.0")))
+  '((request-deferred "0.2.0")
+    (dash "2.11.0")))
