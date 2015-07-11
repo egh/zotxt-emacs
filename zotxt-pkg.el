5c4a55e9c52eeba17b1033e29197ba88b1c26517
Erik Hetzner <egh@e6h.org>
2015-07-11 00:25:31 -0700

    bump version to 0.1.30

--- zotxt-pkg.el
+++ zotxt-pkg.el
@@ -1,4 +1,4 @@
-(define-package "zotxt" "0.1.16"
+(define-package "zotxt" "0.1.30"
   "Tools to integrate emacs with Zotero via the zotxt plugin."
   '((request-deferred "0.2.0")
     (dash "2.11.0")))
