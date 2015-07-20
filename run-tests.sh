470aaf871093f8fc7da23aa60af6d28529921faf
Erik Hetzner <egh@e6h.org>
2015-07-19 23:38:43 -0700

    Test compilation

--- run-tests.sh
+++ run-tests.sh
@@ -1,4 +1,11 @@
-#/bin/sh -e
+#/bin/sh -ex
+
+# ensure no compile errors
+rm ./*.elc
+cask exec emacs -Q --batch -L . --eval '(setq byte-compile-error-on-warn t)' \
+     -f batch-byte-compile zotxt.el
+cask exec emacs -Q --batch -L . --eval '(setq byte-compile-error-on-warn t)' \
+     -f batch-byte-compile org-zotxt.el
 bundle install --quiet
 cask
 ruby mock-server.rb -p 33119  &
@@ -6,8 +13,8 @@ MOCK_PID=$!
 sleep 2
 cask exec ert-runner
 cask exec ecukes
-kill $MOCK_PID
-while (kill -0 $MOCK_PID > /dev/null 2> /dev/null) ; do
+kill "$MOCK_PID"
+while (kill -0 "$MOCK_PID" > /dev/null 2> /dev/null) ; do
     sleep 1
 done
     
