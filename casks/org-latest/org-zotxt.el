7b8a9e5afad6fe62717b9dcb92517c416cde0ac7
Erik Hetzner <egh@e6h.org>
2017-01-06 22:45:09 -0800

    Restructure tests to allow testing multiple org versions

---
+++ casks/org-latest/org-zotxt.el
@@ -0,0 +1,1 @@
+../../org-zotxt.el
--- run-tests.sh
+++ run-tests.sh
@@ -1,18 +1,24 @@
 #/bin/sh -ex
 
-# ensure no compile errors
-rm -f ./*.elc
-cask
-cask exec emacs -Q --batch -L . --eval '(setq byte-compile-error-on-warn t)' \
-     -f batch-byte-compile zotxt.el
-cask exec emacs -Q --batch -L . --eval '(setq byte-compile-error-on-warn t)' \
-     -f batch-byte-compile org-zotxt.el
 bundle install --quiet
 ruby mock-server.rb -p 33119  &
 MOCK_PID=$!
 sleep 2
-cask exec ert-runner
-cask exec ecukes
+
+for CASK_DIR in casks/* ; do
+    cd "$CASK_DIR" || exit
+    cask
+    # ensure no compile errors
+    rm -f ./*.elc
+    cask exec emacs -Q --batch -L . --eval '(setq byte-compile-error-on-warn t)' \
+         -f batch-byte-compile zotxt.el
+    cask exec emacs -Q --batch -L . --eval '(setq byte-compile-error-on-warn t)' \
+         -f batch-byte-compile org-zotxt.el
+    cask exec ert-runner
+    cask exec ecukes
+    cd ../.. || exit
+done
+
 kill "$MOCK_PID"
 while (kill -0 "$MOCK_PID" > /dev/null 2> /dev/null) ; do
     sleep 1
