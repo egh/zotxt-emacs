a36829b1b648fe7a843731c4908067bb3aee3518
Erik Hetzner <egh@e6h.org>
2017-01-08 10:58:21 -0800

    Improve run-tests.sh
    - Quiet ecukes/ert
    - Quiet shell
    - Quiet mock server
    - Misc cleanup

--- run-tests.sh
+++ run-tests.sh
@@ -1,21 +1,22 @@
-#/bin/sh -ex
+#/bin/sh -e
 
 bundle install --quiet
-ruby mock-server.rb -p 33119  &
+ruby mock-server.rb -p 33119 >/dev/null 2>/dev/null &
 MOCK_PID=$!
 sleep 2
 
 for CASK_DIR in casks/* ; do
+    printf "Testing %s\n" "$(basename "${CASK_DIR}")"
     cd "$CASK_DIR" || exit
+    cask clean-elc
     cask
     # ensure no compile errors
-    rm -f ./*.elc
     cask exec emacs -Q --batch -L . --eval '(setq byte-compile-error-on-warn t)' \
          -f batch-byte-compile zotxt.el
     cask exec emacs -Q --batch -L . --eval '(setq byte-compile-error-on-warn t)' \
          -f batch-byte-compile org-zotxt.el
-    cask exec ert-runner
-    cask exec ecukes
+    cask exec ert-runner --quiet
+    cask exec ecukes --quiet
     cd ../.. || exit
 done
 
